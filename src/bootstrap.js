47a985dace6424cf91e4a6e83660349a40230d5e
Wenderson <wps713@gmail.com>
2020-01-31 13:58:09 -0300

    Using common js syntax in bootstrap

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,5 +1,3 @@
-import dotenv from 'dotenv';
-
-dotenv.config({
-  path: process.env.NODE === 'test' ? '.env.test' : '.env',
+require('dotenv').config({
+  path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env',
 });
