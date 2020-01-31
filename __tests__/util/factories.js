0810a0294fbe72a39ec951596ba1b69730777615
Wenderson <wps713@gmail.com>
2020-01-31 18:30:03 -0300

    Add recipient in factory girl attrs

--- __tests__/util/factories.js
+++ __tests__/util/factories.js
@@ -2,11 +2,22 @@ import faker from 'faker';
 import { factory } from 'factory-girl';
 
 import User from '../../src/app/models/User';
+import Recipient from '../../src/app/models/Recipient';
 
 factory.define('User', User, {
   name: faker.name.findName(),
   email: faker.internet.email(),
   password: faker.internet.password(),
+});
+
+factory.define('Recipient', Recipient, {
+  name: faker.name.findName(),
+  street: faker.address.streetName(),
+  number: 578,
+  complement: faker.address.streetAddress(),
+  city: faker.address.city(),
+  state: faker.address.state(),
+  cep: 64000000,
 });
 
 export default factory;
