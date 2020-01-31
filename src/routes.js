705db16a02838fdf10207737a0c36c5750c3632c
Wenderson <wps713@gmail.com>
2020-01-31 17:13:43 -0300

    Add authMiddleware in routes

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,15 @@ import { Router } from 'express';
 import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 routes.get('/', (req, res) => res.json({ message: 'Hello world' }));
+
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
 
 export default routes;
