d402809e22c1d2409becaa8c246f5ecbe5b0b9ec
Wenderson <wps713@gmail.com>
2020-01-31 18:31:17 -0300

    Add recipient test

---
+++ __tests__/integration/controller/recipient.test.js
@@ -0,0 +1,23 @@
+import request from 'supertest';
+import app from '../../../src/app';
+
+import truncate from '../../util/truncate';
+import factory from '../../util/factories';
+
+describe('Recipient', () => {
+  beforeAll(async () => {
+    await truncate();
+  });
+
+  it('should not be able to complete the request without name', async () => {
+    const recipient = await factory.attrs('Recipient');
+
+    recipient.name = '';
+
+    const response = await request(app)
+      .post(`/recipient`)
+      .send(recipient);
+
+    expect(response.status).toBe(400);
+  });
+});
