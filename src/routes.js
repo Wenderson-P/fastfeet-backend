823af67efb3cbbc23002c3aecd09e8244d599816
Wenderson <wps713@gmail.com>
2020-02-03 19:22:58 -0300

    Setting up file route in routes

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,17 @@
 import { Router } from 'express';
 
+import multer from 'multer';
+import multerConfig from './config/multer';
+
 import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
 import RecipientController from './app/controllers/RecipientController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.get('/', (req, res) => res.json({ message: 'Hello world' }));
 
@@ -18,4 +23,5 @@ routes.use(authMiddleware);
 routes.post('/recipients', RecipientController.store);
 routes.put('/recipients/:id', RecipientController.update);
 
+routes.post('/files', upload.single('file'), FileController.store);
 export default routes;
