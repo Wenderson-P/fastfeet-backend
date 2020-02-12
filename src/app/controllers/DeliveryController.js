34808846384c37f1a08a7c27d351c04cc6724c25
Wenderson <wps713@gmail.com>
2020-02-12 15:40:06 -0300

    Add delete method in delivery ctrl

--- src/app/controllers/DeliveryController.js
+++ src/app/controllers/DeliveryController.js
@@ -143,7 +143,19 @@ class DeliveryController {
     });
   }
 
-  async delete(req, res) { }
+  async delete(req, res) {
+    const { id } = req.params;
+
+    const delivery = await Delivery.findByPk(id);
+
+    if (!delivery) {
+      return res.status(400).json({ error: 'Delivery does not exists.' });
+    }
+
+    await delivery.destroy();
+
+    return res.json('Delivery was deleted');
+  }
 }
 
 export default new DeliveryController();
