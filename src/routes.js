39af08e4ba562cb5ff06da493a88d3f550e412ee
Wenderson <wps713@gmail.com>
2020-04-13 20:33:42 -0300

    Changing auth middleware requirement

--- src/routes.js
+++ src/routes.js
@@ -21,6 +21,19 @@ routes.get('/', (req, res) => res.json({ message: 'Hello world' }));
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+routes.get('/deliveryman/:id', DeliverymanController.show);
+
+routes.get('/deliveryman/:id/deliveries', DeliveriesController.index);
+routes.put(
+  '/deliveryman/:id/deliveries',
+  upload.single('file'),
+  DeliveriesController.update
+);
+
+routes.get('/delivery/:id/problems', DeliveryProblemController.index);
+routes.post('/delivery/:id/problems', DeliveryProblemController.store);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 routes.use(authMiddleware);
 
@@ -28,8 +41,6 @@ routes.get('/recipients', RecipientController.index);
 routes.post('/recipients', RecipientController.store);
 routes.put('/recipients/:id', RecipientController.update);
 routes.delete('/recipients/:id', RecipientController.delete);
-
-routes.post('/files', upload.single('file'), FileController.store);
 
 routes.get('/deliveryman', DeliverymanController.index);
 routes.post('/deliveryman', DeliverymanController.store);
@@ -41,15 +52,7 @@ routes.post('/delivery', DeliveryController.store);
 routes.put('/delivery/:id', DeliveryController.update);
 routes.delete('/delivery/:id', DeliveryController.delete);
 
-routes.get('/deliveryman/:id/deliveries', DeliveriesController.index);
-routes.put(
-  '/deliveryman/:id/deliveries',
-  upload.single('file'),
-  DeliveriesController.update
-);
 routes.get('/delivery/problems', DeliveryProblemController.index);
-routes.get('/delivery/:id/problems', DeliveryProblemController.index);
-routes.post('/delivery/:id/problems', DeliveryProblemController.store);
 routes.put('/problem/:id/cancel-delivery', DeliveryProblemController.update);
 
 export default routes;
