400fcd4a5cbf822c85d483d1b6d85163a1142af3
Wenderson <wps713@gmail.com>
2020-04-15 13:03:01 -0300

    Ad nodemailer config
    You need to change this to your config.

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '0302e244265b45',
+    pass: '36f569c4fbb217',
+  },
+  default: {
+    from: 'FastFeet <naoresponda@fastfeet.com>',
+  },
+};
