6b5db0180933f61bc2e570543e75a6e980eebb17
Wenderson <wps713@gmail.com>
2020-04-15 12:41:23 -0300

    Add created_add field in delivery man query

--- src/app/controllers/DeliverymanController.js
+++ src/app/controllers/DeliverymanController.js
@@ -48,9 +48,13 @@ class DeliverymanController {
     const { id } = req.params;
 
     const deliveryman = await Deliveryman.findByPk(id, {
-      attributes: ['id', 'name', 'email'],
+      attributes: ['id', 'name', 'email', 'created_at'],
       include: [
-        { model: File, as: 'avatar', attributes: ['name', 'path', 'url'] },
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['name', 'path', 'url'],
+        },
       ],
     });
 
